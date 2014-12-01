a2e3344f9a9d09b816e8a7d401e53fb4d334e29b
ath01 <darkwawe00@gmail.com>
2014-12-02 01:32:53 +0500

    add

---
+++ src/ArraysSort2.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by darkwawe on 02.12.2014.
+ */
+public class ArraysSort2 {
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        int [] array = new int[5];
+        for (int i = 0; i < array.length; i++) {
+            array[i] =  Integer.parseInt(br.readLine());
+        }
+        int temp;
+        int temp2;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i]<array[j]){
+                    temp = array[j];
+                    array[j] = array[i];
+                    array[i] = temp;
+
+                }
+
+
+            }
+        }
+        for (int i : array) {
+            System.out.println(i);
+        }
+
+    }
+}
